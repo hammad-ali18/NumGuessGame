912c0705736770e860859631e84d3ad7d0a0bcc3
Hammad <hammadalipoison@icloud.com>
2023-02-09 16:10:20 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NumGuessGame
+ 
---
+++ index.ts
@@ -0,0 +1,32 @@
+import inquirer from 'inquirer'
+
+type ansType ={
+    userGuess:number
+}
+
+const systemGeneratedNo = Math.ceil(Math.random()*10);
+
+
+var count =5;
+while(count >0){
+const answers:ansType = await inquirer.prompt([
+    {
+        type:'number',
+        name:'userGuess',
+        message:'Write your Guess: '
+    }
+])
+
+    const {userGuess} = answers;//destructuring
+if(userGuess === systemGeneratedNo){
+    console.log(userGuess, "userGuess",systemGeneratedNo);
+    
+    console.log("Yess your answere is Correct\n YOU WIN!");
+    
+}else{
+    console.log("You LOOSE \n TRY AGAIN");
+    
+}
+count --;
+}
+console.log("~~~~~THE ACTUAL ANS WAS :",systemGeneratedNo+" ~~~~~~");
